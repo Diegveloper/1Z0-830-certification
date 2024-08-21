84c249358ef528d59faaf8e917b9288d6145723c
Your Name <diegod9405@gmail.com>
2024-08-21 10:05:21 -0600

    donts

--- src/main/java/ozomahtli/generics/Overloading.java
+++ src/main/java/ozomahtli/generics/Overloading.java
@@ -1,4 +1,20 @@
 package ozomahtli.generics;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Overloading {
+
+}
+
+class LongTailAnimal{
+    protected void chew(List<Object> input){}
+}
+
+class Anteater extends LongTailAnimal{
+    //Won't compile due to mismatch in generic
+    //protected void chew(List<Double> input){}
+
+    //This works because it overloads
+    protected void chew(ArrayList<Double> inpout){}
 }
